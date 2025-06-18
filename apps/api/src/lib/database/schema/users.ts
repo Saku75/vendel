5e8d7eb7a84dff9abc3bb224638590b13391318f
Lukas Vendel Mann <lukas@lvmann.io>
2025-06-18 21:41:45 +0000

    Create user table

---
+++ apps/api/src/lib/database/schema/users.ts
@@ -0,0 +1,53 @@
+import { createId } from "@paralleldrive/cuid2";
+import {
+  AnySQLiteColumn,
+  int,
+  sqliteTable,
+  text,
+} from "drizzle-orm/sqlite-core";
+
+import { AuthRole } from "$lib/enums/auth/role";
+
+const usersSchema = sqliteTable("users", {
+  // Internal ID
+  id: text({ length: 24 })
+    .notNull()
+    .primaryKey()
+    .$default(() => createId()),
+
+  // Name
+  firstName: text({ length: 64 }).notNull(),
+  middleName: text({ length: 256 }),
+  lastName: text({ length: 64 }),
+
+  // Credentials
+  email: text({ length: 320 }).notNull().unique(),
+  emailVerified: int({ mode: "boolean" }).notNull().default(false),
+  password: text({ length: 128 }),
+  clientSalt: text({ length: 64 }).notNull(),
+  serverSalt: text({ length: 64 }).notNull(),
+
+  // Role
+  role: text({
+    length: 16,
+    enum: [AuthRole.SuperAdmin, AuthRole.Admin, AuthRole.User, AuthRole.Guest],
+  }),
+
+  // Approval
+  approved: int({ mode: "boolean" }).notNull().default(false),
+  approvedBy: text({ length: 24 }).references(
+    (): AnySQLiteColumn => usersSchema.id,
+    { onUpdate: "cascade", onDelete: "set null" },
+  ),
+
+  // Timestamps
+  createdAt: int({ mode: "timestamp" })
+    .notNull()
+    .$default(() => new Date()),
+  updatedAt: int({ mode: "timestamp" })
+    .notNull()
+    .$default(() => new Date())
+    .$onUpdate(() => new Date()),
+});
+
+export { usersSchema };
