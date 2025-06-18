0ac286e3c168d23d9e542f983f9ef85cf0abfd5c
Lukas Vendel Mann <lukas@lvmann.io>
2025-06-18 21:48:23 +0000

    Add email worker functionality

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -30,4 +30,28 @@ export default {
 
     return await server.fetch(request, env, context);
   },
+  async email(message) {
+    const forwardingMails: {
+      alias: string;
+      forwardTo: string;
+    }[] = [
+      { alias: "gert@vendel.dk", forwardTo: "gertvendelmann@proton.me" },
+      { alias: "jens@vendel.dk", forwardTo: "jensvendellarsen@gmail.com" },
+      { alias: "julie@vendel.dk", forwardTo: "julievendel@hotmail.com" },
+      { alias: "karina@vendel.dk", forwardTo: "karinavendel@gmail.com" },
+      { alias: "lukas@vendel.dk", forwardTo: "lvmann@proton.me" },
+      { alias: "susan@vendel.dk", forwardTo: "susanvendel@gmail.com" },
+    ];
+
+    const forwardingMail = forwardingMails.find(
+      (value) => value.alias === message.to.toLocaleLowerCase(),
+    );
+
+    if (!forwardingMail) {
+      message.setReject("Email does not exist");
+      return;
+    }
+
+    await message.forward(forwardingMail.forwardTo);
+  },
 } satisfies ExportedHandler<CloudflareBindings>;
