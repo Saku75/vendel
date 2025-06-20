43d716789fb12273f562586bca8ea4114e98ffe9
Lukas Vendel Mann <lukas@lvmann.io>
2025-06-20 10:56:03 +0000

    Implement validator package and remove password length in database

---
+++ packages/validators/src/main.ts
@@ -0,0 +1,13 @@
+import { email } from "./validators/email";
+import { id } from "./validators/id";
+import { name } from "./validators/name";
+import { password } from "./validators/password";
+
+const validators = {
+  email,
+  id,
+  name,
+  password,
+};
+
+export { validators };
---
+++ packages/validators/src/validators/name.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+const name = {
+  first: z
+    .string({
+      required_error: "Fornavn skal udfyldes.",
+      invalid_type_error: "Fornavn skal være tekst.",
+    })
+    .nonempty("Fornavn skal udfyldes.")
+    .max(50, "Fornavn må højst være 50 tegn."),
+
+  middle: z
+    .string({
+      required_error: "Mellemnavn skal udfyldes.",
+      invalid_type_error: "Mellemnavn skal være tekst.",
+    })
+    .max(200, "Mellemnavn må højst være 200 tegn."),
+
+  last: z
+    .string({
+      required_error: "Efternavn skal udfyldes.",
+      invalid_type_error: "Efternavn skal være tekst.",
+    })
+    .max(50, "Efternavn må højst være 50 tegn."),
+};
+
+export { name };
