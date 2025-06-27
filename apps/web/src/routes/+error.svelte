19c522985ac44cc2600e3dae570cf847ee64ea72
Lukas Vendel Mann <lukas@lvmann.io>
2025-06-27 12:53:31 +0000

    Add error handling page

---
+++ apps/web/src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<main class="grid place-items-center text-center">
+  <h1 class="px-4 text-4xl font-medium sm:text-5xl md:text-6xl">
+    {page.error?.message}
+  </h1>
+</main>
