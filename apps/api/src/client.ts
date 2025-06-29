7044512a34ade0084a17a6b5b7b83bde145a0cd3
Lukas Vendel Mann <lukas@lvmann.io>
2025-06-29 16:38:11 +0000

    Export schema and endpoint types from api

--- apps/api/src/client.ts
+++ apps/api/src/client.ts
@@ -12,3 +12,17 @@ type Client = typeof client;
 
 export { createClient };
 export type { Client };
+
+export type {
+  SignInFinishRequest,
+  SignInSession,
+  SignInStartRequest,
+  SignInStartResponse,
+} from "$routes/auth/sign-in";
+export type {
+  SignUpFinishRequest,
+  SignUpSession,
+  SignUpStartRequest,
+  SignUpStartResponse,
+} from "$routes/auth/sign-up";
+export type { WhoAmIResponse } from "$routes/auth/who-am-i";
