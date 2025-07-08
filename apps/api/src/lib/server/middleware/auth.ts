607e98be31c74beb4a1d86f2ad1edc62352962ab
Lukas Vendel Mann <lukas@lvmann.io>
2025-07-08 06:44:52 +0000

    Handle old tokens with unexpected content by removing them and logging out the user

--- apps/api/src/lib/server/middleware/auth.ts
+++ apps/api/src/lib/server/middleware/auth.ts
@@ -12,37 +12,42 @@ import { getAuthSession } from "$lib/utils/auth/session";
 const authMiddleware = createMiddleware<HonoEnv>(async (c, next) => {
   const authCookie = getAuthCookie(c);
 
-  if (authCookie && authCookie.valid) {
-    const { user, refreshToken } = authCookie.payload.data;
+  try {
+    if (authCookie && authCookie.valid) {
+      const { user, refreshToken } = authCookie.payload.data;
 
-    c.set("auth", {
-      status: authCookie.expired
-        ? AuthStatus.Expired
-        : AuthStatus.Authenticated,
-      authToken: { expiresAt: authCookie.payload.expiresAt },
-      refreshToken,
-      user,
-    });
-  } else {
-    if ((authCookie && !authCookie.valid) || authCookie === null) {
-      deleteAuthCookie(c);
-      deleteAuthRefreshCookie(c);
-    }
+      c.set("auth", {
+        status: authCookie.expired
+          ? AuthStatus.Expired
+          : AuthStatus.Authenticated,
+        authToken: { expiresAt: authCookie.payload.expiresAt },
+        refreshToken,
+        user,
+      });
+    } else {
+      if ((authCookie && !authCookie.valid) || authCookie === null) {
+        deleteAuthCookie(c);
+        deleteAuthRefreshCookie(c);
+      }
 
-    c.set("auth", { status: AuthStatus.Unauthenticated });
-  }
-
-  if (authCookie) {
-    const { refreshToken } = authCookie.payload.data;
-
-    const authSession = await getAuthSession(c, refreshToken.id);
-
-    if (authSession?.refreshToken.used) {
       c.set("auth", { status: AuthStatus.Unauthenticated });
-
-      deleteAuthCookie(c);
-      deleteAuthRefreshCookie(c);
     }
+
+    if (authCookie) {
+      const { refreshToken } = authCookie.payload.data;
+
+      const authSession = await getAuthSession(c, refreshToken.id);
+
+      if (authSession?.refreshToken.used) {
+        c.set("auth", { status: AuthStatus.Unauthenticated });
+
+        deleteAuthCookie(c);
+        deleteAuthRefreshCookie(c);
+      }
+    }
+  } catch {
+    deleteAuthCookie(c);
+    deleteAuthRefreshCookie(c);
   }
 
   await next();
