226da827809b78efd88afbc005f1e2c372789f73
Lukas Vendel Mann <lukas@lvmann.io>
2025-11-08 14:29:27 +0000

    Chore: Update dependencies

--- packages/token/eslint.config.js
+++ packages/token/eslint.config.js
@@ -2,11 +2,4 @@ import { defineConfig } from "eslint/config";
 
 import { customConfig } from "@config/eslint";
 
-export default defineConfig(customConfig(), {
-  languageOptions: {
-    parserOptions: {
-      tsconfigRootDir: import.meta.dirname,
-      project: true,
-    },
-  },
-});
+export default defineConfig(customConfig());
