253f80d29dfb823964be1a216a544c7b99f8dc35
Lukas Vendel Mann <lukas@lvmann.io>
2025-11-25 13:47:25 +0000

    Fix syntax error in refresh_tokens schema

--- apps/api_new/src/lib/database/migrations/0008_black_the_initiative.sql
+++ apps/api_new/src/lib/database/migrations/0008_black_the_initiative.sql
@@ -11,7 +11,7 @@ CREATE TABLE `refresh_tokens` (
 	`id` text(24) PRIMARY KEY NOT NULL,
 	`refresh_token_family_id` text(24) NOT NULL,
 	`used` integer DEFAULT false NOT NULL,
-	`expires_at` integer DEFAULT (unixepoch()) + 2592000 NOT NULL,
+	`expires_at` integer DEFAULT (unixepoch() + 2592000) NOT NULL,
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
 	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
 	FOREIGN KEY (`refresh_token_family_id`) REFERENCES `refresh_token_families`(`id`) ON UPDATE cascade ON DELETE cascade
