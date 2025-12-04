82fd7af7d31d1f25813f8b9460c5274b01ab7880
Lukas Vendel Mann <lukas@lvmann.io>
2025-12-04 09:37:02 +0000

    Only refresh tokens on page refresh when tokens require a refresh

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -1,3 +1,5 @@
+import { TokenExpiresIn, TokenService } from "@package/token-service";
+
 import { npm_package_version } from "$env/static/private";
 
 import { LayoutTheme } from "$lib/enums/layout/theme";
@@ -5,14 +7,7 @@ import { LayoutTheme } from "$lib/enums/layout/theme";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ platform, cookies, locals }) => {
-  const authCookiesPresent = Boolean(
-    cookies.get(`${platform?.env.COOKIE_PREFIX}-access`) &&
-      cookies.get(`${platform?.env.COOKIE_PREFIX}-refresh`),
-  );
-
-  const refresh = authCookiesPresent && (await locals.api.auth.refresh());
-  const whoAmI =
-    refresh && refresh.ok ? await locals.api.user.whoAmI() : undefined;
+  const whoAmI = await getAuthenticatedUser(platform!, cookies, locals);
 
   return {
     config: {
@@ -22,6 +17,39 @@ export const load: LayoutServerLoad = async ({ platform, cookies, locals }) => {
       canonicalOrigin: platform!.env.CANONICAL_ORIGIN,
       turnstileSiteKey: platform!.env.TURNSTILE_SITE_KEY,
     },
-    whoAmI: whoAmI && whoAmI.ok ? whoAmI.data : undefined,
+    whoAmI,
   };
 };
+
+async function getAuthenticatedUser(
+  platform: App.Platform,
+  cookies: Parameters<LayoutServerLoad>[0]["cookies"],
+  locals: App.Locals,
+) {
+  const cookiePrefix = platform.env.COOKIE_PREFIX;
+  const accessCookie = cookies.get(`${cookiePrefix}-access`);
+  const refreshCookie = cookies.get(`${cookiePrefix}-refresh`);
+
+  if (!accessCookie || !refreshCookie) {
+    return undefined;
+  }
+
+  const { metadata } = await locals.tokenService.read(accessCookie, {
+    metadataOnly: true,
+  });
+
+  const expiringThreshold = TokenService.getExpiresAt(
+    TokenExpiresIn.FiveMinutes,
+  );
+  const shouldRefresh = metadata.expiresAt <= expiringThreshold;
+
+  if (shouldRefresh) {
+    const refreshResult = await locals.api.auth.refresh();
+    if (!refreshResult.ok) {
+      return undefined;
+    }
+  }
+
+  const result = await locals.api.user.whoAmI();
+  return result.ok ? result.data : undefined;
+}
